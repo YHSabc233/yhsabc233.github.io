16e1864635d2d554033c642c623d5f02f502ed38
YHSabc233 <yhsabc233@gmail.com>
2026-01-17 23:48:02 +0800

    重置：恢复到原来的个人网站

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # YHSabc233 的网站
 
-需要打开网站？
-点击这里：[www.yhsabc233.top](https://www.yhsabc233.top/)
-
----
-
-# YHSabc233's website
-
-need to open the website?
-click here:[www.yhsabc233.top](https://www.yhsabc233.top/)
-
----
-
-**更新时间：2025/11/04**
-
-上次更新：2025/10/22
-
-*上上次更新：2025/10/19*
+你可以通过 [www.yhsabc233.top](https://www.yhsabc233.top/) 来访问该网站。
--- files/js/AudioPlayer.js
+++ files/js/AudioPlayer.js
@@ -3,20 +3,18 @@ function AudioPlayer(AudioName) {
     const audio = new Audio();
 
     audio.src = "/files/audio/" + AudioName
-    audio.play();
+    audio.play()
 }
 
 function CialloAudioPlayer() {
-    var min = 0; var max = 15; var randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-    
+    const min = 0;
+    const max = 7;
+    const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
     if(randomNum <= 3){
-        AudioPlayer("meguru_ciallo.mp3");
+        AudioPlayer("meguru_ciallo.mp3")
     } else if(randomNum >= 4 && randomNum <= 7){
-        AudioPlayer("yoshino_ciallo.mp3");
-    } else if(randomNum >= 8 && randomNum <= 11){
-        AudioPlayer("mayu_ciallo.mp3");
-    } else if(randomNum >= 12){
-        AudioPlayer("fumika_ciallo.mp3");
+        AudioPlayer("yoshino_ciallo.mp3")
     }
 }
 
